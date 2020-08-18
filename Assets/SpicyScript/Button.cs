6ceeaedc8a7cd729eb8a1add2211498145526c43
Adrian Hui <adrianhui519@gmail.com>
2020-08-17 20:00:31 -0700

    Spicy's stuff
    added main menu stuff

---
+++ Assets/SpicyScript/Button.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer[] Sprites;
+    [SerializeField] private int IndexOfNextScene;
+
+    public UnityEvent Clicked;
+    
+    private void OnMouseEnter() 
+    {
+        foreach(SpriteRenderer Sprite in Sprites)
+        {
+            Sprite.color = Color.grey;
+        }
+    }
+
+    private void OnMouseExit() 
+    {
+        foreach(SpriteRenderer Sprite in Sprites)
+        {
+            Sprite.color = Color.white;
+        }
+    }
+    void OnMouseDown()
+    {
+        foreach(SpriteRenderer Sprite in Sprites)
+        {
+            Sprite.color = Color.red;
+        }
+        Clicked.Invoke();
+    }
+
+    void OnMouseUp() 
+    {        
+        foreach(SpriteRenderer Sprite in Sprites)
+        {
+            Sprite.color = Color.grey;
+        }
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(IndexOfNextScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/SpicyScript/MoveLogo.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveLogo : MonoBehaviour
+{
+    [SerializeField] private float WhereToStop = 0;
+    [SerializeField] private float Second = 1f;
+    [SerializeField] private GameObject Startbutton;
+    [SerializeField] private GameObject Stopbutton;
+    private bool DoOnce = true;
+
+    void Awake()
+    {
+        transform.position = new Vector2(-14.5f, 30.5f);
+        if(Startbutton != null && Stopbutton != null)
+        {
+            Startbutton.SetActive(false);
+            Stopbutton.SetActive(false);
+        }
+        StartCoroutine(Move(WhereToStop));
+    }
+
+    IEnumerator Move (float StopPos)
+    {
+        Debug.Log("Coroutine started");
+        while(transform.position.y != StopPos)
+        {
+            yield return new WaitForSeconds(Second);
+            transform.position = new Vector2(transform.position.x, transform.position.y - 1f);
+        }
+        yield return new WaitForSeconds(0.3f);
+        if(Startbutton != null && Stopbutton != null)
+        {
+            Startbutton.SetActive(true);
+            Stopbutton.SetActive(true);
+        }
+        StopCoroutine(Move(WhereToStop));
+        Debug.Log("Coroutine stopped");
+    }
+}
